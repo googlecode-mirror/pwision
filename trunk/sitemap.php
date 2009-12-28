68b46b64bec10e8081e0d687f526f37855c1b4b4
becheru.petru.ioan@gmail.com <becheru.petru.ioan@gmail.com@86788e72-db30-11de-b12c-574f4db5f7e4>
2009-12-28 15:10:30 +0000

    * minor change

--- trunk/sitemap.php
+++ trunk/sitemap.php
@@ -1,4 +1,22 @@
 <?php
+/*
+part of PWision toolkit: http://pwision.googlecode.com/
+Copyright (C) 2009 Becheru Petru-Ioan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 include_once("pwision.inc");
 class PMySitemap extends PSitemap
 	{
